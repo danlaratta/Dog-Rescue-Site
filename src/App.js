68f9aa71c558ab193a8e065f3cfda250db48037c
Dan <dan.laratta.jr@gmail.com>
2021-09-11 12:46:39 -0400

    init setup still

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
+      
       <Router>
         <Switch>
           <Route exact path='/' component={} />
