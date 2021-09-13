ca78f53636667bcf9f56989fb99e7a87a4a667d7
Dan <dan.laratta.jr@gmail.com>
2021-09-13 17:21:49 -0400

    home page started

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import '../styles/Home.css'
 
 const Home = () => {
     return (
-        <div>
-            Home Page
-        </div>
+        <>
+            <div className="home">
+                
+            </div>
+        </>
     )
 }
 
