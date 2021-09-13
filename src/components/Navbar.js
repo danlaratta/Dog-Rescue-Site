2b4eee97e2af23ab9c29d02989e39734739e0384
Dan <dan.laratta.jr@gmail.com>
2021-09-13 12:07:52 -0400

    info bar at top

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,43 +15,67 @@ const Navbar = () => {
     }
 
     return (
-        <nav className="navbar">
-            <Link to='/' className="logo"> Dog Rescue </Link>
+        <>
+            <div className="info-bar">
+                <div className="info-bar-items">
+                    <h1 className="info-text">
+                        <i className="fas fa-mobile-alt"></i> (973) 123-456 
+                    </h1>
+                </div>
 
-            <ul className={mobileNav ? 'nav-mobile' : 'nav-menu'}>
-                <li className="nav-items">
-                    <Link to='/' className="nav-links" onClick={handleCloseMobileNav}> HOME </Link>
-                </li>
+                <div className="info-bar-items">
+                    <h1 className="info-text">
+                        <i className="fas fa-map-marker-alt"></i>  123 DOG RESCUE LANE, TOMS RIVER, NJ 08753 
+                    </h1>
+                </div>
 
-                <li className="nav-items">
-                    <Link to='/about-us' className="nav-links" onClick={handleCloseMobileNav}> ABOUT US </Link>
-                </li>
-
-                <li className="nav-items">
-                    <Link to='/adopt' className="nav-links" onClick={handleCloseMobileNav}> ADOPT </Link>
-                </li>
-
-                <li className="nav-items">
-                    <Link to='/events' className="nav-links" onClick={handleCloseMobileNav}> EVENTS </Link>
-                </li>
-
-                <li className="nav-items">
-                    <Link to='/volunteer' className="nav-links" onClick={handleCloseMobileNav}> VOLUNTEER </Link>
-                </li>
-
-                <li className="nav-items">
-                    <Link to='/donate' className="nav-links" onClick={handleCloseMobileNav}> DONATE </Link>
-                </li>
-
-                <li className="nav-items">
-                    <Link to='/contact-us' className="nav-links" onClick={handleCloseMobileNav}> CONTACT US </Link>
-                </li>
-            </ul>
-
-            <div className="mobile-icon" onClick={handleMobileIconClick}>
-                <i className={mobileNav ? 'fas fa-times' : 'fas fa-bars'}/>
+                <div className="info-bar-items">
+                    <ul className="social-media-icons">
+                        <li className="icon"> <i class="fab fa-facebook"></i> </li>
+                        <li className="icon"> <i class="fab fa-instagram"></i> </li>
+                        <li className="icon"> <i class="fab fa-twitter"></i> </li>
+                    </ul>
+                </div>
             </div>
-        </nav>
+
+            <nav className="navbar">
+                <Link to='/' className="logo"> Dog Rescue </Link>
+
+                <ul className={mobileNav ? 'nav-mobile' : 'nav-menu'}>
+                    <li className="nav-items">
+                        <Link to='/' className="nav-links" onClick={handleCloseMobileNav}> HOME </Link>
+                    </li>
+
+                    <li className="nav-items">
+                        <Link to='/about-us' className="nav-links" onClick={handleCloseMobileNav}> ABOUT US </Link>
+                    </li>
+
+                    <li className="nav-items">
+                        <Link to='/adopt' className="nav-links" onClick={handleCloseMobileNav}> ADOPT </Link>
+                    </li>
+
+                    <li className="nav-items">
+                        <Link to='/events' className="nav-links" onClick={handleCloseMobileNav}> EVENTS </Link>
+                    </li>
+
+                    <li className="nav-items">
+                        <Link to='/volunteer' className="nav-links" onClick={handleCloseMobileNav}> VOLUNTEER </Link>
+                    </li>
+
+                    <li className="nav-items">
+                        <Link to='/donate' className="nav-links" onClick={handleCloseMobileNav}> DONATE </Link>
+                    </li>
+
+                    <li className="nav-items">
+                        <Link to='/contact-us' className="nav-links" onClick={handleCloseMobileNav}> CONTACT US </Link>
+                    </li>
+                </ul>
+
+                <div className="mobile-icon" onClick={handleMobileIconClick}>
+                    <i className={mobileNav ? 'fas fa-times' : 'fas fa-bars'}/>
+                </div>
+            </nav>
+        </>
     )
 }
 
