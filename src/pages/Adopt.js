2a1f0493c69e20afc95e24e9dddf4ca1121a1aed
Dan <dan.laratta.jr@gmail.com>
2021-09-20 20:45:06 -0400

    started adopt page

--- src/pages/Adopt.js
+++ src/pages/Adopt.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import '../styles/Adopt.css'
+import Creatable from 'react-select/creatable'
+import Axios from 'axios'
 
 const Adopt = () => {
+
+    const [dog, setDog] = useState('')
+    const [count, setCount] = useState(0)
+
+    const handleCount = () => {
+        setCount(count + 1)
+    }
+
+    useEffect(() => {
+        Axios.get('https://dog.ceo/api/breeds/list/all').then((res) => {
+            console.log(res.data.message.beagle)
+        })
+    }, [count])
+    
     return (
-        <div>
-            Adopt Page
-        </div>
+        <>
+            <button onClick={handleCount}> Click me </button>
+        </>
     )
 }
 
