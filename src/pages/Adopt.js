b402e2ec6e0ca22cfc9b22eba76e8cabf623a545
Dan <dan.laratta.jr@gmail.com>
2021-09-28 00:33:24 -0400

    working on dog details on hover

--- src/pages/Adopt.js
+++ src/pages/Adopt.js
@@ -6,17 +6,26 @@ import SearchBar from '../components/SearchBar'
 const Adopt = () => {
 
     const [dogs, setDogs] = useState([])
-    const [searchedBreed, setSearchedBreed] = useState('hound')   
+    const [searchedBreed, setSearchedBreed] = useState('hound')  
+    const [dogName, setDogName] = useState('') 
 
     const getData = async () => {
 
-        const url = `https://dog.ceo/api/breed/${searchedBreed}/images` 
+        const dogsUrl = `https://dog.ceo/api/breed/${searchedBreed}/images` 
+        const namesUrl = 'https://randomuser.me/api/'
 
-        const response = await Axios.get(url)
+        const responseDogs = await Axios.get(dogsUrl)
+        const responseNames = await Axios.get(namesUrl)
         
         // check if it received proper data then set the state to received value 
-        if(response.data.message){
-            setDogs(response.data.message)
+        if(responseDogs.data.message){
+            setDogs(responseDogs.data.message)
+        }
+
+        const nameData = responseNames.data.results[0].name.first
+
+        if(nameData){
+            setDogName(nameData)
         }
     }
 
@@ -36,6 +45,11 @@ const Adopt = () => {
                 {dogs.slice(0, 16).map((dog) => (
                     <div className="dog-item" key={dog}>
                         <img src={dog} id="dog-img" alt="dog img" />
+
+                        <div className="dog-info">
+                            <h1 className="dog-name"> { dogName } </h1>
+                            <h1 className="dog-age"> </h1>
+                        </div>
                     </div>
                 ))}
             </div>
