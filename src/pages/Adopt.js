b7d4c070a4b470118ff759dff4f18848d905928a
Dan <dan.laratta.jr@gmail.com>
2021-09-28 23:29:01 -0400

    trying to get name api to out of loop its stuck in

--- src/pages/Adopt.js
+++ src/pages/Adopt.js
@@ -2,35 +2,29 @@ import React, { useState, useEffect } from 'react'
 import '../styles/Adopt.css'
 import Axios from 'axios'
 import SearchBar from '../components/SearchBar'
+import DogDetails from '../components/DogDetails'
 
 const Adopt = () => {
 
     const [dogs, setDogs] = useState([])
     const [searchedBreed, setSearchedBreed] = useState('hound')  
-    const [dogName, setDogName] = useState('') 
+     
 
-    const getData = async () => {
-
+    const getDogData = async () => {
         const dogsUrl = `https://dog.ceo/api/breed/${searchedBreed}/images` 
-        const namesUrl = 'https://randomuser.me/api/'
 
         const responseDogs = await Axios.get(dogsUrl)
-        const responseNames = await Axios.get(namesUrl)
+        
+        const dogData = responseDogs.data.message
         
         // check if it received proper data then set the state to received value 
-        if(responseDogs.data.message){
-            setDogs(responseDogs.data.message)
-        }
-
-        const nameData = responseNames.data.results[0].name.first
-
-        if(nameData){
-            setDogName(nameData)
+        if(dogData){
+            setDogs(dogData)
         }
     }
 
     useEffect(() => {
-        getData()
+        getDogData()
     }, [searchedBreed])
     
     return (
@@ -44,11 +38,8 @@ const Adopt = () => {
             <div className="dogs-container">
                 {dogs.slice(0, 16).map((dog) => (
                     <div className="dog-item" key={dog}>
-                        <img src={dog} id="dog-img" alt="dog img" />
-
-                        <div className="dog-info">
-                            <h1 className="dog-name"> { dogName } </h1>
-                            <h1 className="dog-age"> </h1>
+                        <div className="dog-info" style={{backgroundImage: `url(${dog})`}}>
+                            
                         </div>
                     </div>
                 ))}
